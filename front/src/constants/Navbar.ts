5ba990b3918d1b0e4d3368e927c11eff8f37298f
Bruno Giugno <brunogiugno@gmail.com>
2025-08-06 10:30:04 -0300

    dependencias listas

---
+++ front/src/constants/Navbar.ts
@@ -0,0 +1,21 @@
+// src/constants/navbarLinks.ts
+import { NavbarItemProps } from "@/components/ui/Navbar/NavbarItem";
+
+export const navbarLinks: NavbarItemProps[] = [
+{
+    label: "Página principal",
+    href: "/",
+},
+{
+    label: "Paleta de sabores",
+    href: "/paleta-de-sabores",
+},
+{
+    label: "Carrito de compras",
+    href: "/carrito-de-compras",
+},
+{
+    label: "Perfil",
+    href: "/perfil",
+},
+];
