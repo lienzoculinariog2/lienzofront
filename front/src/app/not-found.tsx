8775f81bf3f34b9bc67bc94edddb2242309ccb39
Joaco Curbelo <joacoc-10@hotmail.com>
2025-08-07 15:19:57 +0200

    Pagina de error

---
+++ front/src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import Button from "@/components/ui/Button";
+import Link from "next/link";
+
+export default function NotFound() {
+
+  return (
+    <>
+      <div className="min-h-[calc(100vh-var(--header-height,0px)-var(--footer-height,0px))] flex flex-col items-center justify-center text-center py-16 px-4">
+        <h1 className="mb-6 font-extrabold text-daily-menu-600 text-8xl md:text-9xl">
+          404
+        </h1>
+        <p className="max-w-md mb-8 text-xl font-semibold text-gray-700 md:text-2xl dark:text-gray-300">
+          ¡Oops! Parece que la página que estás buscando no existe.
+        </p>
+        <Link href="/">
+          <Button
+            variant="dailyMenu"
+            className="px-16 py-4 text-lg rounded-lg "
+          > Volver al Inicio</Button>
+        </Link>
+      </div>
+    </>
+  )
+}
