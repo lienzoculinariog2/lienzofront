267209a1fc1a79d2f5dc8d984effec8f142e0b96
Bruno Giugno <brunogiugno@gmail.com>
2025-08-06 21:39:59 -0300

    nav y footer terminado

--- front/src/components/ui/Navbar/Navbar.tsx
+++ front/src/components/ui/Navbar/Navbar.tsx
@@ -3,47 +3,49 @@ import Link from "next/link";
 import NavbarItem from "./NavbarItem";
 import { navbarLinks } from "@/constants/Navbar";
 
-const isUserLogged = false; 
+const isUserLogged = false;
 
 export default function Navbar() {
 return (
-    <nav className="bg-primary-background-900 text-secondary-txt-400 shadow-md">
-    <div className="container mx-auto px-4 py-3 flex justify-center items-center">
+    <div className="bg-primary-background-500">
+
+      {/* Tu navbar sin el borde de abajo */}
+    <nav className="text-secondary-txt-400 shadow-md">
+        <div className="container mx-auto px-4 py-3 flex justify-center items-center">
         
-        {/* Todos los links están juntos y centrados */}
+          {/* Todos los links están juntos y centrados */}
         <ul className="flex items-center space-x-6">
-          {/* El enlace de Home ahora es un item más de la lista */}
-        <li>
-            <Link 
-            href="/" 
-            className="uppercase font-semibold text-sm hover:text-primary-txt-100 transition-colors"
-            >
-            Home
-            </Link>
-        </li>
-        
-          {/* Los demás links */}
-        {navbarLinks.map((item) => (
-            <NavbarItem 
-            key={item.label}
-            label={item.label}
-            href={item.href}
-            />
-        ))}
-            
-          {/* Link condicional para Iniciar sesión */}
-        {!isUserLogged && (
             <li>
             <Link
-                href="/iniciar-sesion"
+                href="/"
                 className="uppercase font-semibold text-sm hover:text-primary-txt-100 transition-colors"
             >
-                Iniciar sesión
+                Home
             </Link>
             </li>
-        )}
+            
+            {navbarLinks.map((item) => (
+            <NavbarItem
+                key={item.label}
+                label={item.label}
+                href={item.href}
+            />
+            ))}
+            
+            {!isUserLogged && (
+            <li>
+                <Link
+                href="/iniciar-sesion"
+                className="uppercase font-semibold text-sm hover:text-primary-txt-100 transition-colors"
+                >
+                Iniciar sesión
+                </Link>
+            </li>
+            )}
         </ul>
-    </div>
+        </div>
     </nav>
+    <div className="h-1 w-full bg-gradient-to-r from-secondary-background-900 to-secondary-txt-400"></div>
+    </div>
 );
 }
